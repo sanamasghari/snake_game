649bf8a52837a7be984c1164dce13b1f223831df
sanam <sn9721683@gmail.com>
2025-12-14 21:36:34 +0330

    feat (root): complete methods and classes and logic

--- main.py
+++ main.py
@@ -1,8 +1,69 @@
 from snake import Snake, Food, Game
+import sys
+
 
 def main():
-    ...
+    # Board-Map:
+    map_width = 17
+    map_height = 15
+    
+    # board = []
+    # for _ in range(map_height):
+    #     length = []
+    #     for _ in range(map_width):
+    #         length.append(0)
+    #     board.append(length)
+        
+    board = [[0 for _ in range(map_height)] for _ in range(map_height)]
+        
+    #initial snake location:
+    snake_body = [[6,0], [6,1], [6,2]]
+    snake_direction = "right"
+    snake = Snake(snake_body, snake_direction)
+    
+    #initial apple location:    
+    apple = Food()
+    apple.random_location(map_width, map_height)
+    while apple.food_location in snake_body:
+        apple.random_location(map_width, map_height)
 
+    #initialize Game
+    game = Game(board, snake, apple, map_width, map_height)
 
+        
+    while not game.game_over:
+        
+        board = [[0 for _ in range(map_height)] for _ in range(map_height)]
+
+        apple_row, apple_col = apple.food_location
+        board[apple_row][apple_col] = "🍎"
+
+        for r,c in snake_body:
+            board[r][c] = "🐍"
+            
+        #input
+        user_direction = input()
+        #check direction
+        snake.check_direction(user_direction)
+        # check eaten food 
+        if game.control_eat() == "food-eaten":
+            snake.move(tail_addition=True)
+            game.tail_addition=False
+            print("score:", game.score)
+        else:
+        # move (flag)
+            snake.move(tail_addition=False)        
+            
+        # check collision
+        game.control_hit()
+        if game.game_over:
+            break
+        
+        print(board)
+            
+        
+    
 if __name__ == "__main__":
     main()
+    
+    
--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ This file consists features and logic of the game using OOP (classes)
 
 """
 
+import random
 
 class Snake:
     # body: The coordinates of snake body 
@@ -11,41 +12,89 @@ class Snake:
         self.body = body
         self.direction = direction
 
+
+    def check_direction(self, new_direction):
+        opposite_direction = {"up":"down", "down":"up", "right":"left", "left":"right"}
+        
+        if new_direction != opposite_direction[self.direction]:
+            self.direction = new_direction
+        
+        
+    def update_direction(self, key):
+        
+        
     # Move snake one step to selected direction 
-    def move(self):
-        pass
-
-    # Add one more unit to tail of snake after eating food 
-    def grow(self):
-        pass
-
+    def move(self, tail_addition = False):
+        row, col = self.body[0]
+        
+        match self.direction:
+            case "right":
+                col = col + 1
+            case "up":
+                row = row - 1
+            case "left":
+                col = col - 1
+            case "down":
+                row = row + 1
+                
+        new_head = [row, col] 
+        self.body.insert(0, new_head)
+            
+        # Add one more unit to tail of snake after eating food 
+        if not tail_addition:
+            self.body.pop()
+            
 
 class Food:
-    def __init__(self, location):
+    def __init__(self, food_location = None):
         # location: coordinate of the food
-        self.location = location
+        self.food_location = food_location
 
     # Choose random food location and put it on a coordinate of the map
-    def random_location(self):
-        pass
+    def random_location(self, map_width, map_height):
+        food_row = random.randint(0, map_width - 1)
+        food_col = random.randint(0, map_height - 1)
+        
+        self.food_location = [food_row, food_col]
 
 
 class Game:
     #controls the Game logic
-    def __init__(self, board, snake, food):
+    def __init__(self, board, snake, food, map_width, map_height, score = 0):
         self.board = board
         self.snake = snake
         self.food = food
         self.game_over = False
+        self.map_width = map_width
+        self.map_height = map_height
+        self.score = score
+        self.tail_addition = False
 
     def control_eat(self):
         # Check head location with food location to check if it ate the food
-        pass
+        if self.food.food_location == self.snake.body[0]:
+            self.score += 1
+            self.tail_addition = True      
+            while True:
+                self.food.random_location(self.map_width, self.map_height)
+                if self.food.food_location in self.snake.body:
+                    continue
+                else:
+                    break         
+            return "food-eaten"
+
+
 
     def control_hit(self):
         # Check head location with location of walls around the region or its body 
-        pass
-
-    def control_end(self):
-        # Check whether the game should end
-        pass
+        # 1. Self-Collision:
+        for i in self.snake.body[1:]:
+            if i == self.snake.body[0]:
+                self.game_over = True
+             
+        
+        # 2. Wall-collision
+        row_head, col_head = self.snake.body[0]
+        if row_head >= self.map_height or row_head < 0 or col_head >= self.map_width or col_head < 0:
+            self.game_over = True
+        
